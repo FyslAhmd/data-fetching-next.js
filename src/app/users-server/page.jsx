cbb378f6dbf3b2e9da3f8a55f154a56b296b00ec
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-31 01:44:26 +0600

    handle error and loading in data fetching

--- src/app/users-server/page.jsx
+++ src/app/users-server/page.jsx
@@ -3,7 +3,6 @@ export default async function UsersServer() {
 
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await response.json();
-  console.log(users);
 
   return (
     <ul className="space-y-4 p-4">
